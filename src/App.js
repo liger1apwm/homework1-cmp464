ce73422b620f9e14906cff00918cd3fb787d3037
Eric <ericaragundi@gmail.com>
2022-02-24 15:59:58 -0500

    modified App.js to say Eric

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <h1>JSX IS SO COOL!!!</h1> */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        {/* <p> YOOO we are in reactttt.</p> */}
+        <p> My name is Eric!! and I'm going to master React!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
